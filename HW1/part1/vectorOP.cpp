30cb65112a13bf9ea19d0757b6cb007849fb8d20
5000user5000 <ck10600760@gmail.com>
2025-09-19 17:48:25 +0800

    feat: constructed all feature in part1

--- HW1/part1/vectorOP.cpp
+++ HW1/part1/vectorOP.cpp
@@ -11,6 +11,8 @@ void absVector(float *values, float *output, int N)
   //  Note: Take a careful look at this loop indexing.  This example
   //  code is not guaranteed to work when (N % VECTOR_WIDTH) != 0.
   //  Why is that the case?
+  // Becasue, when N is not a multiple of VECTOR_WIDTH, the last iteration
+  // will access out-of-bound memory locations.
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
 
@@ -38,6 +40,16 @@ void absVector(float *values, float *output, int N)
     // Write results back to memory
     _pp_vstore_float(output + i, result, maskAll);
   }
+
+  // Handle remaining elements at the end when N is not a multiple of VECTOR_WIDTH
+  for(int i = (N / VECTOR_WIDTH) * VECTOR_WIDTH; i < N; i++) {
+    float x = values[i];
+    if (x < 0) {
+      output[i] = -x;
+    } else {
+      output[i] = x;
+    }
+  }
 }
 
 void clampedExpVector(float *values, int *exponents, float *output, int N)
@@ -48,9 +60,78 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
   //
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
-  //
+  
+  __pp_vec_float vx;
+  __pp_vec_int vy;
+
+  __pp_vec_float res = _pp_vset_float(0.f);;
+  __pp_vec_int zero = _pp_vset_int(0);
+  __pp_vec_int ones = _pp_vset_int(1);
+
+  __pp_mask maskAll, maskIsZero, maskIsNotZero;
+
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
+    // init mask
+    maskAll = _pp_init_ones();
+    maskIsZero = _pp_init_ones(0);
+    
+    // load
+    _pp_vload_float(vx, values + i, maskAll);
+    _pp_vload_int(vy, exponents + i, maskAll);
+    
+    // check exp as 0 or not
+    _pp_veq_int(maskIsZero, vy, zero, maskAll);
+
+    // if exp == 0
+    _pp_vset_float(res, 1.f, maskIsZero);
+
+    // else
+    maskIsNotZero = _pp_mask_not(maskIsZero);
+    _pp_vmove_float(res, vx, maskIsNotZero); // res = x
+  
+    while(_pp_cntbits(maskIsNotZero) > 0){
+      _pp_vmult_float(res, res, vx, maskIsNotZero); // res *= x
+      _pp_vsub_int(vy, vy,ones, maskIsNotZero); // exp -= 1
+      _pp_veq_int(maskIsZero, vy, zero, maskIsNotZero);
+      maskIsNotZero = _pp_mask_not(maskIsZero);
+    }
+
+    // clamp
+    __pp_vec_float limit = _pp_vset_float(9.999999f);
+    __pp_mask maskIsOverLimit = _pp_init_ones(0);
+    _pp_vgt_float(maskIsOverLimit, res, limit, maskAll);
+    _pp_vset_float(res, 9.999999f, maskIsOverLimit);
+    
+    // store
+    _pp_vstore_float(output + i, res, maskAll);
+
+  }
+
+  // remaining part
+  for(int i = (N / VECTOR_WIDTH) * VECTOR_WIDTH; i < N; i++){
+    // from serialOP.cpp
+    float x = values[i];
+    int y = exponents[i];
+    if (y == 0)
+    {
+      output[i] = 1.f;
+    }
+    else
+    {
+      float result = x;
+      int count = y - 1;
+      while (count > 0)
+      {
+        result *= x;
+        count--;
+      }
+      if (result > 9.999999f)
+      {
+        result = 9.999999f;
+      }
+      output[i] = result;
+    }
   }
 }
 
@@ -64,9 +145,34 @@ float arraySumVector(float *values, int N)
   // PP STUDENTS TODO: Implement your vectorized version of arraySumSerial here
   //
 
+  __pp_vec_float sum = _pp_vset_float(0.f);
+  __pp_vec_float x;
+  __pp_mask maskAll, maskIsNegative, maskIsNotNegative;
+
+  float res = 0.0;
+
+
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
+    // All ones
+    maskAll = _pp_init_ones();
+
+    // load new vars
+    _pp_vload_float(x, values + i, maskAll);
+
+    // accum into sum (veca can be same as vecResult,right?)
+    _pp_vadd_float(sum,sum,x,maskAll);
   }
 
-  return 0.0;
+  // sum of the vector
+  for(int i=0; i < VECTOR_WIDTH; i++){
+    res += x.value[i]; // n.b. no 's' in value
+  }
+
+  // for remaining elements
+  for(int i = (N / VECTOR_WIDTH) * VECTOR_WIDTH; i < N; i++){
+    res += values[i];
+  }
+
+  return res;
 }
