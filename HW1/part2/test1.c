1815155d05648a3a2c84f53365f5f196a09ea36a
5000user5000 <ck10600760@gmail.com>
2025-10-03 20:23:32 +0800

    簡化

--- HW1/part2/test1.c
+++ HW1/part2/test1.c
@@ -2,10 +2,7 @@
 #include <stdint.h>
 
 void test1(float *__restrict a, float *__restrict b, float *__restrict c, int N) {
-  __builtin_assume(N == 1024);          // assume N%8 ==0 （AVX2: 8 floats/vec）
-  __builtin_assume((uintptr_t)a % 32 == 0);
-  __builtin_assume((uintptr_t)b % 32 == 0);
-  __builtin_assume((uintptr_t)c % 32 == 0);
+  __builtin_assume(N == 1024);
 
   a = (float *)__builtin_assume_aligned(a, 32);  // 32-byte for AVX2
   b = (float *)__builtin_assume_aligned(b, 32);
