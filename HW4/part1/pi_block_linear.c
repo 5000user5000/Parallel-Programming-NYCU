b796ec7e22d8e85563b68797b2606fbc6185711a
5000user5000 <ck10600760@gmail.com>
2025-11-03 23:26:20 +0800

    完成 1.2.1 - 完成 monte carlo method - rank > 0, MPI_Send - rank = 0 則用 for loop 接收所有,並計算結果

--- HW4/part1/pi_block_linear.c
+++ HW4/part1/pi_block_linear.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SEED 1234
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -16,19 +18,41 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Calculate local count for this process
+    long long int number_of_tosses = tosses / world_size;
+    long long int count = 0;
+    unsigned int seed = world_rank * SEED;
+
+    for (long long int toss = 0; toss < number_of_tosses; toss++)
+    {
+        double x = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;
+        double y = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;
+        if (x * x + y * y <= 1.0)
+        {
+            count++;
+        }
+    }
 
     if (world_rank > 0)
     {
-        // TODO: handle workers
+        // Workers send their count to rank 0
+        MPI_Send(&count, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
-        // TODO: main
-    }
+        // Rank 0 receives counts from all other processes and accumulates
+        long long int received_count;
+        for (int i = 1; i < world_size; i++)
+        {
+            MPI_Recv(&received_count, 1, MPI_LONG_LONG, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            count += received_count;
+        }
 
-    if (world_rank == 0)
-    {
-        // TODO: process PI result
+        // Calculate PI from total count
+        pi_result = 4.0 * (double)count / (double)tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
