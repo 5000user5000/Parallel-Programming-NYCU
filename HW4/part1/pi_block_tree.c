f028abb5b295c507521ab6150fe677a8bb777bf9
5000user5000 <ck10600760@gmail.com>
2025-11-04 17:10:10 +0800

    refactor: 把 monte carlo 計算挪到外部 func

--- HW4/part1/pi_block_tree.c
+++ HW4/part1/pi_block_tree.c
@@ -7,21 +7,7 @@
 
 #define SEED 1234
 
-int main(int argc, char **argv)
-{
-    // --- DON'T TOUCH ---
-    MPI_Init(&argc, &argv);
-    double start_time = MPI_Wtime();
-    double pi_result;
-    long long int tosses = atoi(argv[1]);
-    int world_rank, world_size;
-    // ---
-
-    // TODO: MPI init
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    
+long long int compute_pi(long long int tosses, int world_rank, int world_size){
     // Calculate local count for this process
     long long int number_of_tosses = tosses / world_size;
     long long int count = 0;
@@ -40,6 +26,24 @@ int main(int argc, char **argv)
             count++;
         }
     }
+    return count;
+}
+
+int main(int argc, char **argv)
+{
+    // --- DON'T TOUCH ---
+    MPI_Init(&argc, &argv);
+    double start_time = MPI_Wtime();
+    double pi_result;
+    long long int tosses = atoi(argv[1]);
+    int world_rank, world_size;
+    // ---
+
+    // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    long long int count = compute_pi(tosses, world_rank, world_size);
 
     // TODO: binary tree redunction
     int step = 1;
